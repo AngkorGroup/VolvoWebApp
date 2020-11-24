89e2ec794e4c6946301b07b761726528ecd31a2a
mawcam <m.castanedam@pucp.pe>
2020-11-23 20:35:42 -0500

    Removed feature of refetch on focus window for react query

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import GuardedRoute from 'common/components/GuardedRoute/GuardedRoute';
 import moment from 'moment';
 import React, { Suspense } from 'react';
+import { QueryCache, ReactQueryCacheProvider } from 'react-query';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import { AppProvider } from './AppContext';
@@ -18,37 +19,49 @@ const theme = createMuiTheme({
 	...pageTheme,
 });
 
+const queryCache = new QueryCache({
+	defaultConfig: {
+		queries: {
+			refetchOnWindowFocus: false,
+		},
+	},
+});
+
 const App = () => {
 	return (
 		<div className='App'>
-			<ThemeProvider theme={theme}>
-				<MuiPickersUtilsProvider libInstance={moment} utils={MomentUtils}>
-					<AppProvider>
-						<Router>
-							<Suspense fallback={<CircularProgress size={40} thickness={4} />}>
-								<Switch>
-									{routes.map((route: RouteType) => (
-										<Route
-											key={route.id}
-											exact
-											path={route.path}
-											component={route.component}
-										/>
-									))}
-									{guarded.map((route: RouteType) => (
-										<GuardedRoute
-											key={route.id}
-											exact
-											path={route.path}
-											component={route.component}
-										/>
-									))}
-								</Switch>
-							</Suspense>
-						</Router>
-					</AppProvider>
-				</MuiPickersUtilsProvider>
-			</ThemeProvider>
+			<ReactQueryCacheProvider queryCache={queryCache}>
+				<ThemeProvider theme={theme}>
+					<MuiPickersUtilsProvider libInstance={moment} utils={MomentUtils}>
+						<AppProvider>
+							<Router>
+								<Suspense
+									fallback={<CircularProgress size={40} thickness={4} />}
+								>
+									<Switch>
+										{routes.map((route: RouteType) => (
+											<Route
+												key={route.id}
+												exact
+												path={route.path}
+												component={route.component}
+											/>
+										))}
+										{guarded.map((route: RouteType) => (
+											<GuardedRoute
+												key={route.id}
+												exact
+												path={route.path}
+												component={route.component}
+											/>
+										))}
+									</Switch>
+								</Suspense>
+							</Router>
+						</AppProvider>
+					</MuiPickersUtilsProvider>
+				</ThemeProvider>
+			</ReactQueryCacheProvider>
 		</div>
 	);
 };
