27b1a33b8769660f863de77ea61f34bc64c69ca8
mawcam <m.castanedam@pucp.pe>
2020-11-27 14:48:53 -0500

    Fixed validation when add card type

--- src/common/validations/CardType.ts
+++ src/common/validations/CardType.ts
@@ -4,7 +4,7 @@ import yup from './YupLocale';
 interface CardTypeForm {
 	type: string;
 	description: string;
-	currency: string;
+	currencyId: string;
 	term: string;
 	color: string;
 	tpCode: string;
@@ -13,7 +13,7 @@ interface CardTypeForm {
 export const CardTypeSchema = yup.object<CardTypeForm>({
 	type: yup.string().max(10).required(),
 	description: yup.string().max(TEXT_MAX_LENGTH).required(),
-	currency: yup.string().required(),
+	currencyId: yup.string().required(),
 	term: yup.string().matches(/^\d+$/, NUMERIC_FIELD_MESSAGE).max(2).required(),
 	color: yup.string().required(),
 	tpCode: yup.string().matches(/^\d+$/, NUMERIC_FIELD_MESSAGE).required(),
